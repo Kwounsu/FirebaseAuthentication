dc25596934eb68441a7bb747f45c60323adfd528
Kwounsu <sb00lgs@gmail.com>
2020-06-04 09:09:25 -0700

    Facebook

--- app/src/main/java/com/example/authenticationapp/MainActivity.kt
+++ app/src/main/java/com/example/authenticationapp/MainActivity.kt
@@ -4,19 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var googleSignInClient : GoogleSignInClient
     private val RC_SIGN_IN = 1000
+    var callbackManager = CallbackManager.Factory.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +43,12 @@ class MainActivity : AppCompatActivity() {
         googleSignInClient = GoogleSignIn.getClient(this,gso)
 
         button_google.setOnClickListener {
-            var signInIntent = googleSignInClient?.signInIntent
+            val signInIntent = googleSignInClient?.signInIntent
             startActivityForResult(signInIntent,RC_SIGN_IN)
+        }
+
+        button_facebook.setOnClickListener {
+            facebookLogin()
         }
     }
 
@@ -47,6 +59,8 @@ class MainActivity : AppCompatActivity() {
             task ->
             if(task.isSuccessful){
                 Toast.makeText(this,"Success",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this,"Failed",Toast.LENGTH_LONG).show()
             }
         }
     }
@@ -57,12 +71,42 @@ class MainActivity : AppCompatActivity() {
             task ->
             if(task.isSuccessful){
                 Toast.makeText(this,"Success",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this,"Failed",Toast.LENGTH_LONG).show()
             }
         }
     }
+
+    fun facebookLogin(){
+        LoginManager.getInstance().logInWithReadPermissions(this, listOf("public_profile","email"))
+        LoginManager.getInstance().registerCallback(callbackManager,object: FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                firebaseAuthWithFacebook(result)
+            }
+
+            override fun onCancel() {
+            }
+
+            override fun onError(error: FacebookException?) {
+            }
+
+        })
+    }
+    fun firebaseAuthWithFacebook(result: LoginResult?){
+        var credential = FacebookAuthProvider.getCredential(result?.accessToken?.token!!)
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                Toast.makeText(this,"Success",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this,"Failed",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
+        callbackManager.onActivityResult()
         if(requestCode == RC_SIGN_IN){
             var task = GoogleSignIn.getSignedInAccountFromIntent(data)
             var account = task.getResult(ApiException::class.java)
